f5de2b8cc9e04ee4d03e7fe13a564353c219c095
sanadharwish <sanadharwish@gmail.com>
2026-05-16 20:25:27 +0530

    dd

--- happy/assets/script.js
+++ happy/assets/script.js
@@ -40,3 +40,46 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+const form = document.getElementById("enquiry-form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Sending...";
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: json
+        });
+
+        const data = await response.json();
+
+        if (response.status == 200) {
+            result.innerHTML = "Message sent successfully ✅";
+
+            // Clear form fields
+            form.reset();
+
+        } else {
+            result.innerHTML = data.message;
+        }
+
+    } catch (error) {
+        result.innerHTML = "Something went wrong!";
+    }
+
+    setTimeout(() => {
+        result.innerHTML = "";
+    }, 3000);
+});
